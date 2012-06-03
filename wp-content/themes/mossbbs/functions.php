66fee11793504d68f0896d2a5addebb7eeb2c862
voxbono <janvidar@gmail.com>
2012-06-03 03:22:52 +0200

    Fixet litt på custom fields

--- wp-content/themes/mossbbs/functions.php
+++ wp-content/themes/mossbbs/functions.php
@@ -7,21 +7,102 @@
 add_theme_support( 'post-thumbnails' ); 
 
 
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-	register_post_type( 'bil',
+/* Create the 'bil' post type */
+add_action('init', add_post_type('bil'));
+
+
+/* 
+ * Dynamic function for adding a post type
+ * $args is optional, as some default settings are set,
+ * If you want to overwrite some settings, you need to pass in that setting
+ */
+function add_post_type($name, $args = array())
+{
+	$upper = ucwords($name);
+	$name = strtolower(str_replace(' ', '_', $name));
+	
+	$args = array_merge(
 		array(
-			'labels' => array(
-				'name' => __( 'MossBBS' ),
-				'singular_name' => __( 'MossBBS' )
-			),
 			'public' => true,
 			'has_archive' => true,
-			'supports' => array('title','editor','thumbnail','custom-fields')
-		)
+			'label' => "Alle $upper" . 'er',
+			'labels' => array(
+				'add_new_item' => "Legg inn ny $name",
+				'add_new' => "Legg til ny"
+			),
+			'suports' => array('title','editor','thumbnail','custom-fields')
+		),
+		$args
 	);
+	
+	register_post_type($name,$args);
 }
 
+
+
+/* The different fields to be displayed in the bil post type */
+$instillinger = array(
+	0 => array(
+		'id' => 'jvb_year',
+		'name' => 'AArsmodell'
+	),
+	1 => array(
+		'id' => 'jvb_km',
+		'name' => 'Km.Stand'
+	),
+	2 => array(
+		'id' => 'jvb_pris',
+		'name' => 'Pris'
+	),
+);
+
+add_action('add_meta_boxes','create_bil_meta_boxes');
+
+function create_bil_meta_boxes($instillinger)
+{
+	global $instillinger;
+	
+	foreach ($instillinger as $innstilling) {
+		add_meta_box(
+			$innstilling['id'],
+			$innstilling['name'],
+			'jvb_bil_info_cb',
+			'bil',
+			'normal',
+			'high',
+			$innstilling
+		);
+	}
+}
+
+function jvb_bil_info_cb($post,$metabox)
+{
+	global $post;
+	
+	$meta = get_post_meta($post->ID,$metabox['args']['id'],true);
+	
+	?>
+		<input class="widefat" type="text" name="<?php echo $metabox['args']['id']; ?>" id="<?php echo $metabox['args']['id']; ?>" value="<?php echo $meta; ?>"/>
+	<?php
+}
+
+
+add_action('save_post','jvb_save_meta_box');
+
+function jvb_save_meta_box($instillinger)
+{
+	global $instillinger;
+	global $post;
+	
+	foreach ($instillinger as $instilling) {
+		if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+	
+		if( isset($_POST[$instilling['id']]))
+		{
+			update_post_meta($post->ID,$instilling['id'],$_POST[$instilling['id']]);
+		}
+	}
+}
 
 function catch_that_image() {
   global $post, $posts;
